4848fe32b5729b831ce497ac9a0966901cf4f6ce
harujr0616-design <haru.jr.0616@gmail.com>
2026-01-12 02:05:33 +0900

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -3,7 +3,7 @@ class Script{
         if(window.innerWidth<768){
             return;
         }
-        this.slider = document.querySelector('.snapbox');
+        this.slider = document.querySelector('.snap_box');
         this.next = document.querySelector('.arrow.right');
         this.prev = document.querySelector('.arrow.left');
         this.bindEvents();
